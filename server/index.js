3eac841e6096d22d05a76e77ee456cd0dd77d088
manuel <manuelczs@outlook.com>
2020-11-23 14:43:39 -0300

    simple api route

--- server/index.js
+++ server/index.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const server = express();
-const app = express.Router();
+const appRouter = express.Router();
 const axios = require('axios');
 
-app.get('/', (req, res) => {
-  res.send('api root');
+server.get('/', (req, res) => {
+  res.send('Root api');
 });
 
-app.get('/mainnews', (req, res) => {
+server.get('/api', (req, res) => {
   axios
     .get(
       'http://newsapi.org/v2/everything?q=bitcoin&from=2020-10-23&sortBy=publishedAt&apiKey=4007110774184dce9a8ef9ec2d7c286d'
     )
-    .then((response) => res.send(response.data.articles));
+    .then((response) => res.send(response.data));
 });
-
-app.get('/sidenews', (req, res) => {
-  axios
-    .get(
-      'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4007110774184dce9a8ef9ec2d7c286d'
-    )
-    .then((response) => res.send(response.data.articles));
-});
-
-server.use('/api', app);
 
 server.listen(3002, () => {
   console.log('Port 3002');
