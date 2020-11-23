271d38cbab5c7d9c0cd338b8427d0c09a92d5dc6
manuel <manuelczs@outlook.com>
2020-11-23 12:05:47 -0300

    api root url

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const apiRoutes = express.Router();
 const axios = require('axios');
 
+apiRoutes.route('/').get((req, res) => {
+  res.send('Api root');
+});
+
 apiRoutes.route('/newsfront').get((req, res) => {
   axios
     .get(
