31bf645a605dbe4b64fdb0f124c674b96596ea81
manuel <manuelczs@outlook.com>
2020-11-23 14:52:10 -0300

    res.status(200) added

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const server = express();
 const appRouter = express.Router();
 const axios = require('axios');
 
-server.get('/', (req, res) => {
+server.get('/', (req, res, next) => {
   axios
     .get(
       'http://newsapi.org/v2/everything?q=bitcoin&from=2020-10-23&sortBy=publishedAt&apiKey=4007110774184dce9a8ef9ec2d7c286d'
     )
-    .then((response) => res.send(response.data));
+    .then((response) => res.status(200).send(response.data));
 });
 
 server.get('/api', (req, res) => {
