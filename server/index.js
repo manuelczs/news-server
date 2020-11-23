08be437830fd4da73f9e58f53d06726e66c27532
manuel <manuelczs@outlook.com>
2020-11-23 12:38:46 -0300

    refactoring api

--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
 const express = require('express');
-const app = express();
-const apiRoutes = require('./apiRoutes');
+const server = express();
+const app = express.Router();
+const axios = require('axios');
 
 app.get('/', (req, res) => {
-  res.send('Express');
+  res.send('api root');
 });
 
-app.use('/news', apiRoutes);
+app.get('/mainnews', (req, res) => {
+  axios
+    .get(
+      'http://newsapi.org/v2/everything?q=bitcoin&from=2020-10-23&sortBy=publishedAt&apiKey=4007110774184dce9a8ef9ec2d7c286d'
+    )
+    .then((response) => res.send(response.data.articles));
+});
 
-app.listen(3002, () => {
+app.get('/sidenews', (req, res) => {
+  axios
+    .get(
+      'http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4007110774184dce9a8ef9ec2d7c286d'
+    )
+    .then((response) => res.send(response.data.articles));
+});
+
+server.use('/api', app);
+
+server.listen(3002, () => {
   console.log('Port 3002');
 });
