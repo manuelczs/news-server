676806f9a442e0b8f2bcd67a41f55773f8f20de3
manuel <manuelczs@outlook.com>
2020-11-24 11:00:25 -0300

    http changed by https axios.get()

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ server.use(cors());
 server.get('/', (req, res, next) => {
   axios
     .get(
-      'http://newsapi.org/v2/everything?q=bitcoin&from=2020-10-23&sortBy=publishedAt&apiKey=4007110774184dce9a8ef9ec2d7c286d'
+      'https://newsapi.org/v2/everything?q=bitcoin&from=2020-10-23&sortBy=publishedAt&apiKey=4007110774184dce9a8ef9ec2d7c286d'
     )
     .then((response) => res.status(200).send(response.data));
 });
@@ -17,7 +17,7 @@ server.get('/', (req, res, next) => {
 server.get('/api', (req, res) => {
   axios
     .get(
-      'http://newsapi.org/v2/everything?q=bitcoin&from=2020-10-23&sortBy=publishedAt&apiKey=4007110774184dce9a8ef9ec2d7c286d'
+      'https://newsapi.org/v2/everything?q=bitcoin&from=2020-10-23&sortBy=publishedAt&apiKey=4007110774184dce9a8ef9ec2d7c286d'
     )
     .then((response) => res.status(200).send(response.data));
 });
