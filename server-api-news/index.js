c68a599ea47b099d2c08669d90ea6467c66b607a
manuel <manuelczs@outlook.com>
2021-02-02 01:05:39 -0300

    api route changed to /api/news1 and /api/news2

--- server-api-news/index.js
+++ server-api-news/index.js
@@ -3,16 +3,27 @@ const server = express();
 const appRouter = express.Router();
 const axios = require('axios');
 const cors = require('cors');
+const router = express.Router();
 
 server.use(cors());
 
-server.get('/', (req, res) => {
+router.get('/news1', (req, res) => {
   axios
     .get(
       'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4007110774184dce9a8ef9ec2d7c286d'
     )
     .then((response) => res.status(200).send(response.data));
 });
+
+router.get('/news2', (req, res) => {
+  axios
+    .get(
+      'http://newsapi.org/v2/everything?q=tesla&from=2021-01-02&sortBy=publishedAt&apiKey=4007110774184dce9a8ef9ec2d7c286d'
+    )
+    .then((response) => res.status(200).send(response.data));
+});
+
+server.use('/api', router);
 
 server.listen(3002, () => {
   console.log('Port 3002');
