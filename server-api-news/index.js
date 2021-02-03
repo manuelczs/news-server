bbe2cf0e1b03725d8b65b9ec451c0e159c84cf65
manuel <manuelczs@outlook.com>
2021-02-03 16:48:04 -0300

    response with json

--- server-api-news/index.js
+++ server-api-news/index.js
@@ -7,23 +7,23 @@ const API_KEY = '4007110774184dce9a8ef9ec2d7c286d';
 
 server.use(cors());
 
-router.get('/news1', (req, res, next) => {
+router.get('/news1', (req, res) => {
   axios
     .get(
       `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${API_KEY}`
     )
-    .then((response) => res.status(200).send(response.data));
+    .then((response) => res.status(200).json(response.data));
 });
 
-router.get('/news2', (req, res, next) => {
+router.get('/news2', (req, res) => {
   axios
     .get(
       `http://newsapi.org/v2/everything?q=apple&from=2021-02-02&to=2021-02-02&sortBy=popularity&apiKey=${API_KEY}`
     )
-    .then((response) => res.status(200).send(response.data));
+    .then((response) => res.status(200).json(response.data));
 });
 
-server.use('/', router);
+server.use('/api', router);
 
 server.get('/', (req, res) => {
   res.send('Welcome to my API');
