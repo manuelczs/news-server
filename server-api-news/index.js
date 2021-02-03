5c493571619d356e373935d08b6ed3f41b484ea7
manuel <manuelczs@outlook.com>
2021-02-03 01:30:59 -0300

    api_key

--- server-api-news/index.js
+++ server-api-news/index.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const server = express();
-const appRouter = express.Router();
 const axios = require('axios');
 const cors = require('cors');
 const router = express.Router();
-server.use('/', router);
+const API_KEY = '4007110774184dce9a8ef9ec2d7c286d';
+
 server.use(cors());
 
-router.get('/news1', (req, res) => {
+router.get('/news1', (req, res, next) => {
   axios
     .get(
-      'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4007110774184dce9a8ef9ec2d7c286d'
+      `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${API_KEY}`
     )
     .then((response) => res.status(200).send(response.data));
 });
 
-router.get('/news2', (req, res) => {
+router.get('/news2', (req, res, next) => {
   axios
     .get(
-      'http://newsapi.org/v2/everything?q=apple&from=2021-02-02&to=2021-02-02&sortBy=popularity&apiKey=4007110774184dce9a8ef9ec2d7c286d'
+      `http://newsapi.org/v2/everything?q=apple&from=2021-02-02&to=2021-02-02&sortBy=popularity&apiKey=${API_KEY}`
     )
     .then((response) => res.status(200).send(response.data));
 });
+
+server.use('/', router);
 
 server.get('/', (req, res) => {
   res.send('Welcome to my API');
